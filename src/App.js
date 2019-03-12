83facccff6df8e1da68859bfc46e2fbaa45afcb9
Umang Darji <umangrathore70@gmail.com>
2019-03-12 17:46:12 +0530

    Test app with client socket io

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import openSocket from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timestamp: 0
+    }
+    const socket = openSocket('https://socket-server-app-test.herokuapp.com/', {
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: Infinity
+    });
+    socket.on('connect', () => {
+      socket.emit('subscribeToTimer', 1000);
+      socket.on('timer', timestamp => this.setState({
+        timestamp
+      }));
+    });
+    socket.on('disconnect', () => {
+      alert('Your socket is disconnect');
+    });
+  }
+
+
   render() {
+    const { timestamp } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. {timestamp}
           </p>
           <a
             className="App-link"
